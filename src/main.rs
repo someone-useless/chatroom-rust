9b968b99ee96744e88f4c7bc07c1587d3393af89
onelone852 <bcuhah@gmail.com>
2024-07-13 21:14:06 +0800

    Test commit

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use std::net::{Ipv4Addr, SocketAddrV4};
+
 use warp::Filter;
 
 #[tokio::main]
 async fn main() {
     let test = warp::path("hello")
         .map(|| "123");
-    warp::serve(test).run(8080).await;
+    warp::serve(test).run(SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 8080)).await;
 }
