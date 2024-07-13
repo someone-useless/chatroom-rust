c06bcba9b07f1d9ed3fd12d45a0ab835eb2f3cec
onelone852 <bcuhah@gmail.com>
2024-07-14 00:38:27 +0800

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
-use std::net::{Ipv4Addr, SocketAddrV4};
+use std::{
+    net::{Ipv4Addr, SocketAddrV4},
+    sync::{atomic::AtomicI32, Arc},
+};
 
 use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    let test = warp::path("hello")
-        .map(|| "123");
-    warp::serve(test).run(SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 8080)).await;
+    let x = Arc::new(AtomicI32::from(0));
+    let x1 = x.clone();
+    let add = warp::path("add").map(move || {
+        x.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+        ""
+    });
+    let main = warp::path("").map(move || x1.load(std::sync::atomic::Ordering::Relaxed).to_string());
+    warp::serve(main.or(add))
+        .run(SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 8080))
+        .await;
 }
